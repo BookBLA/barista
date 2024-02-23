86ca0ae13d11b8849ba991c8fd50b61b1fd32334
eggmun98 <eggmun98@gmail.com>
2024-02-23 16:55:18 +0900

    feat: font 컴포넌트 구현

--- src/commons/components/CustomScreen/CustomScreen.styles.ts
+++ src/commons/components/CustomScreen/CustomScreen.styles.ts
@@ -4,4 +4,5 @@ import { Platform } from 'react-native';
 
 export const MainView = styled.SafeAreaView`
   ${Platform.OS === 'android' && `margin-top: ${getStatusBarHeight()}px;`}
+  height: 100%;
 `;
