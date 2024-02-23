eacf512c3d8fd8328829ba965fbe6f6435fffd28
eggmun98 <eggmun98@gmail.com>
2024-02-23 14:31:54 +0900

    feat: 로그인 페이지 구현

--- src/commons/components/CustomNavigator/CustomNavigator.tsx
+++ src/commons/components/CustomNavigator/CustomNavigator.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Example from '../../../screens/Example/Example';
 import Example02 from '../../../screens/Example02/Example02';
 import { TapScreens } from '../TapComponent/TapScreens';
 import { CustomScreen } from '../CustomScreen/CustomScreen';
+import Login from '../../../screens/Login/Login';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,14 +12,21 @@ const screens = [
   { name: 'tapScreens', component: TapScreens },
   { name: 'example', component: CustomScreen(Example) },
   { name: 'example02', component: CustomScreen(Example02) },
+  { name: 'login', component: CustomScreen(Login) },
 ];
 
 export const CustomNavigator = () => {
-  return (
+  const [accessToken, setAccessToken] = useState(false);
+
+  return accessToken ? (
     <Stack.Navigator initialRouteName="tapScreens" screenOptions={{ headerShown: false }}>
       {screens.map(({ name, component }) => (
         <Stack.Screen key={name} name={name} component={component} />
       ))}
+    </Stack.Navigator>
+  ) : (
+    <Stack.Navigator initialRouteName="login" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="login" component={CustomScreen(Login)} />
     </Stack.Navigator>
   );
 };
---
+++ src/screens/Login/Login.styles.ts
@@ -0,0 +1,54 @@
+import styled from 'styled-components/native';
+import { colors } from '../../commons/styles/variablesStyles';
+
+export const Wrapper = styled.View`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${colors.background};
+  /* padding: 250px;  */
+`;
+
+export const InnerWrapper = styled.View`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const LogoImage = styled.Image`
+  width: 159px;
+  height: 159px;
+`;
+
+export const TitleWrapper = styled.View`
+  width: 126px;
+  height: 30px;
+  border-radius: 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background-color: ${colors.secondary};
+`;
+
+export const TitleText = styled.Text`
+  font-size: 20px;
+  font-weight: 500;
+  color: ${colors.primary};
+`;
+
+export const SubTitleText = styled.Text`
+  font-size: 14px;
+  font-weight: 200;
+`;
+
+export const SnsText = styled.Text`
+  font-size: 14px;
+  font-weight: 700;
+`;
+
+export const LoginButton = styled.Image`
+  width: 300px;
+  height: 45px;
+`;
