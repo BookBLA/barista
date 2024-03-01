22f6bb4311392cc946b82348f97d4942f3f5e09a
eggmun98 <eggmun98@gmail.com>
2024-03-02 00:18:02 +0900

    refactor: 모달 컴포넌트 수정

--- src/commons/components/CustomModal/ModalButtons/ModalButtons.styles.ts
+++ src/commons/components/CustomModal/ModalButtons/ModalButtons.styles.ts
@@ -2,13 +2,6 @@ import styled from 'styled-components/native';
 import { IStyledProps } from './ModalButtons.types';
 import { colors } from '../../../styles/variablesStyles';
 
-export const ButtonText = styled.Text`
-  color: #fff;
-  font-weight: bold;
-  text-align: center;
-  color: ${({ color }: IStyledProps) => color || 'fff'};
-`;
-
 export const BottomWrapper = styled.View`
   width: 100%;
   display: flex;
