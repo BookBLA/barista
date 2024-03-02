c704b7238de070104207d32d85db6f873b4cba48
banggeunho <panggeunho@gmail.com>
2024-03-02 17:56:11 +0900

    feat: Tap Navigator 이미지 및 스타일 적용

--- src/commons/components/TapComponent/TapScreens.tsx
+++ src/commons/components/TapComponent/TapScreens.tsx
@@ -1,36 +1,76 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../../../screens/Home/Home';
-import { Image } from 'react-native';
-import homeIcon from '../../../../assets/images/icons/home.png';
-import matchingIcon from '../../../../assets/images/icons/matching.png';
-import libraryIcon from '../../../../assets/images/icons/library.png';
+import { Image, Platform } from 'react-native';
+import homeBright from '../../../../assets/images/icons/HomeBright.png';
+import homeDark from '../../../../assets/images/icons/HomeDark.png';
+import matchingDark from '../../../../assets/images/icons/MatchingDark.png';
+import matchingBright from '../../../../assets/images/icons/MatchingBright.png';
+import libraryDark from '../../../../assets/images/icons/LibraryDark.png';
+import libraryBright from '../../../../assets/images/icons/LibraryBright.png';
 import Example from '../../../screens/Example/Example';
 import Example02 from '../../../screens/Example02/Example02';
+import { colors } from '../../styles/variablesStyles';
 
 export const TapScreens = () => {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator initialRouteName="home" screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      initialRouteName="홈"
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: colors.primary,
+        tabBarLabelStyle: {
+          fontSize: 12,
+        },
+        tabBarStyle: {
+          height: '8%',
+          ...Platform.select({
+            ios: {
+              paddingTop: 10,
+              paddingBottom: 15,
+            },
+            android: {
+              paddingTop: 10,
+              paddingBottom: 10,
+            },
+          }),
+        },
+      }}
+    >
       <Tab.Screen
-        name="home"
+        name="홈"
         component={Home}
         options={{
-          tabBarIcon: () => <Image source={homeIcon} resizeMode="contain" />,
+          tabBarIcon: ({ focused }) => (
+            <Image source={focused ? homeDark : homeBright} resizeMode="contain" style={{ width: 19, height: 17 }} />
+          ),
         }}
       />
       <Tab.Screen
-        name="matching"
+        name="매칭"
         component={Example}
         options={{
-          tabBarIcon: () => <Image source={matchingIcon} resizeMode="contain" />,
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? matchingDark : matchingBright}
+              resizeMode="contain"
+              style={{ width: 19, height: 17 }}
+            />
+          ),
         }}
       />
       <Tab.Screen
-        name="library"
+        name="내 서재"
         component={Example02}
         options={{
-          tabBarIcon: () => <Image source={libraryIcon} resizeMode="contain" />,
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={focused ? libraryDark : libraryBright}
+              resizeMode="contain"
+              style={{ width: 19, height: 17 }}
+            />
+          ),
         }}
       />
     </Tab.Navigator>
