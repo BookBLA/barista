3a499400cea06648cf8955b34fea0e7b11553492
eggmun98 <eggmun98@gmail.com>
2024-03-03 18:33:26 +0900

    feat: api 요청 실패 시 에러 모달 구현

--- src/commons/utils/http.api.ts
+++ src/commons/utils/http.api.ts
@@ -1,49 +1,73 @@
 import axios from 'axios';
+import { useErrorMessage } from '../store/useErrorMessage';
 
 export const httpApi = axios.create({ baseURL: process.env.EXPO_PUBLIC_BASE_URL });
 
-const config = {};
-
-export const Get = async <T>(url: string): Promise<T> => {
-  return await httpApi
-    .get(url, config)
-    .then((response) => {
-      return response?.data;
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
+const config = {
+  headers: {
+    'Content-Type': 'application/json',
+    accept: 'application/json',
+  },
 };
 
-export const Post = async <T, D>(url: string, data?: D): Promise<T> => {
-  return await httpApi
-    .post(url, data, config)
-    .then((response) => {
-      return response?.data;
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
+export const Get = async <T>(url: string): Promise<T | undefined> => {
+  try {
+    const response = await httpApi.get<T>(url, config);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      useErrorMessage.getState().setErrorMessage(error.message);
+    }
+    return undefined;
+  }
 };
 
-export const Put = async <T, D>(url: string, data?: D): Promise<T> => {
-  return await httpApi
-    .put(url, data, config)
-    .then((response) => {
-      return response?.data;
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
+export const Post = async <T, D>(url: string, data?: D): Promise<T | undefined> => {
+  try {
+    const response = await httpApi.post<T>(url, data, config);
+    return response.data;
+  } catch (error) {
+    if (error instanceof Error) {
+      useErrorMessage.getState().setErrorMessage(error.message);
+    }
+    return undefined;
+  }
 };
 
-export const Delete = async <T>(url: string): Promise<T> => {
-  return await httpApi
-    .delete(url, config)
-    .then((response) => {
-      return response?.data;
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
+export const Put = async <T, D>(url: string, data?: D): Promise<T | undefined> => {
+  try {
+    const response = await httpApi.put<T>(url, data, config);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      useErrorMessage.getState().setErrorMessage(error.message);
+    }
+    return undefined;
+  }
 };
+
+export const Delete = async <T>(url: string): Promise<T | undefined> => {
+  try {
+    const response = await httpApi.delete<T>(url, config);
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      useErrorMessage.getState().setErrorMessage(error.message);
+    }
+    return undefined;
+  }
+};
+
+// const handleResponse = <T>(response: AxiosResponse<T>): ApiResponse<T> => {
+//   return response;
+// };
+
+// const handleError = <T>(error: unknown): ApiResponse<T> => {
+//   let message = 'An unknown error occurred';
+//   if (error instanceof Error) {
+//     message = error.message;
+//   }
+//   useErrorMessage.getState().setErrorMessage(message);
+
+//   return error;
+// };
