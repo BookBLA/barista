dc8026055cbba5eca5bbf906272cb30455bd8816
eggmun98 <eggmun98@gmail.com>
2024-03-10 22:30:26 +0900

    feat: 알림 페이지 구현

--- src/commons/styles/variablesStyles.ts
+++ src/commons/styles/variablesStyles.ts
@@ -1,18 +1,18 @@
 export const colors = {
   primary: '#1D2E61',
-  primaryActive: '',
-  primaryFocus: '',
   secondary: '#F7E2A4',
-  secondaryActive: '',
-  secondaryFocus: '',
   background: '#F7F4ED',
+
   buttonMain: '#FFF5D6',
-  textGray2: '#A8AAB2',
   buttonPrimary: '#1D2E61',
   buttonYellow: '#FFF5D6',
+
   textBlack: '#000000',
   textYellow: '#FFEBAC',
   textGray: '#858585',
+  textGray2: '#A8AAB2',
+  textGray4: '#787878',
+
   errorMessageRed: '#F04C4C',
 };
 
