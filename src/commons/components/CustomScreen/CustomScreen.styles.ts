f42fe3b678440cc519261bda134a3814f45821b5
eggmun98 <eggmun98@gmail.com>
2024-03-10 16:33:55 +0900

    refactor: customScreen 수정

--- src/commons/components/CustomScreen/CustomScreen.styles.ts
+++ src/commons/components/CustomScreen/CustomScreen.styles.ts
@@ -1,8 +1,4 @@
 import styled from 'styled-components/native';
-import { getStatusBarHeight } from 'react-native-status-bar-height';
-import { Platform } from 'react-native';
-
 export const MainView = styled.SafeAreaView`
-  ${Platform.OS === 'android' && `margin-top: ${getStatusBarHeight()}px;`}
   height: 100%;
 `;
