1c1dfbbca84d0ec51df28e2dcd47deb76a9e554b
eggmun98 <eggmun98@gmail.com>
2024-03-10 22:28:03 +0900

    refactor: customText 컴포넌트 margin 기능 추가

--- src/commons/components/CustomText/CustomText.styles.ts
+++ src/commons/components/CustomText/CustomText.styles.ts
@@ -1,16 +1,13 @@
 import styled from 'styled-components/native';
 import { IProps } from './CustomText.Types';
 
+export const Wrapper = styled.View`
+  margin: ${({ margin }: IProps) => margin || 0};
+`;
+
 export const CustomText = styled.Text`
   font-size: ${({ size }: IProps) => size || '16px'};
   color: ${({ color }: IProps) => color || '#000'};
   font-family: ${({ font }: IProps) => font || 'fontMedium'};
   font-weight: ${({ weight }: IProps) => weight || 400};
 `;
-
-// export const CustomText = styled.Text<IProps>`
-//   font-size: ${({ size }) => size || '14px'};
-//   color: ${({ color }) => color || '#000'};
-//   font-family: ${({ font }) => font || 'fontMedium'};
-//   font-weight: ${({ weight }) => weight || 400};
-// `;
