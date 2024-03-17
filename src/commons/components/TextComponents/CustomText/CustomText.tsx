7db27b52a049045a881fe3a9ce52648b5d9d2a9e
banggeunho <panggeunho@gmail.com>
2024-03-17 19:08:46 +0900

    feat: custom text에 style 속성 추가

--- src/commons/components/TextComponents/CustomText/CustomText.tsx
+++ src/commons/components/TextComponents/CustomText/CustomText.tsx
@@ -1,9 +1,9 @@
 import * as S from './CustomText.styles';
 import { IProps } from './CustomText.types';
 
-export const CustomText: React.FC<IProps> = ({ children, size, color, font, weight, margin }) => {
+export const CustomText: React.FC<IProps> = ({ children, size, color, font, weight, margin, style }) => {
   return (
-    <S.CustomText size={size} color={color} font={font} weight={weight} margin={margin}>
+    <S.CustomText size={size} color={color} font={font} weight={weight} margin={margin} style={style}>
       {children}
     </S.CustomText>
   );
