a0261100d1b591a7029a87f1eb33480b14586976
eggmun98 <eggmun98@gmail.com>
2024-03-17 20:00:09 +0900

    feat: customText 버튼 기능 추가

--- src/commons/components/TextComponents/CustomText/CustomText.tsx
+++ src/commons/components/TextComponents/CustomText/CustomText.tsx
@@ -1,10 +1,22 @@
+import React from 'react';
 import * as S from './CustomText.styles';
 import { IProps } from './CustomText.types';
 
-export const CustomText: React.FC<IProps> = ({ children, size, color, font, weight, margin, style }) => {
+export const CustomText = <T extends unknown[] = []>({
+  children,
+  size,
+  color,
+  font,
+  weight,
+  margin,
+  style,
+  onPress,
+}: IProps<T>) => {
   return (
-    <S.CustomText size={size} color={color} font={font} weight={weight} margin={margin} style={style}>
-      {children}
-    </S.CustomText>
+    <S.Wrapper onPress={onPress} margin={margin}>
+      <S.CustomText size={size} color={color} font={font} weight={weight} style={style}>
+        {children}
+      </S.CustomText>
+    </S.Wrapper>
   );
 };
