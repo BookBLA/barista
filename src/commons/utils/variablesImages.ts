15e2af0b4fd379b08b656927b393ecd2199945e3
eggmun98 <eggmun98@gmail.com>
2024-03-31 18:59:34 +0900

    feat: image 파일 모듈화

---
+++ src/commons/utils/variablesImages.ts
@@ -0,0 +1,75 @@
+export const icons = {
+  answerA: require('../../../assets/images/icons/AnswerA.png'),
+  answerB: require('../../../assets/images/icons/AnswerB.png'),
+  answerC: require('../../../assets/images/icons/AnswerC.png'),
+
+  back: require('../../../assets/images/icons/Back.png'),
+  backArrow: require('../../../assets/images/icons/BackArrow.png'),
+  bang: require('../../../assets/images/icons/Bang.png'),
+  bellOff: require('../../../assets/images/icons/BellOff.png'),
+  bellOn: require('../../../assets/images/icons/BellOn.png'),
+  bookmark: require('../../../assets/images/icons/Bookmark.png'),
+  bottomArrow: require('../../../assets/images/icons/BottomArrow.png'),
+
+  check: require('../../../assets/images/icons/Check.png'),
+  checkboxOff: require('../../../assets/images/icons/CheckboxOff.png'),
+  checkboxOn: require('../../../assets/images/icons/CheckboxOn.png'),
+  circle: require('../../../assets/images/icons/Circle.png'),
+  close: require('../../../assets/images/icons/close.png'),
+
+  emptyPostcard: require('../../../assets/images/icons/EmptyPostcard.png'),
+
+  goToTop: require('../../../assets/images/icons/GoToTop.png'),
+
+  homeBlack: require('../../../assets/images/icons/HomeBlack.png'),
+  homeBright: require('../../../assets/images/icons/HomeBright.png'),
+  homeDark: require('../../../assets/images/icons/HomeDark.png'),
+
+  libraryBright: require('../../../assets/images/icons/LibraryBright.png'),
+  libraryDark: require('../../../assets/images/icons/LibraryDark.png'),
+  libraryTransparent: require('../../../assets/images/icons/LibraryTransparent.png'),
+  loading: require('../../../assets/images/icons/loading.png'),
+  lock: require('../../../assets/images/icons/Lock.png'),
+
+  man: require('../../../assets/images/icons/Man.png'),
+  manSmall: require('../../../assets/images/icons/ManSmall.png'),
+  matchingBright: require('../../../assets/images/icons/MatchingBright.png'),
+  matchingDark: require('../../../assets/images/icons/MatchingDark.png'),
+  matchingTransparent: require('../../../assets/images/icons/MatchingTransparent.png'),
+
+  nextArrow: require('../../../assets/images/icons/NextArrow.png'),
+
+  optionA: require('../../../assets/images/icons/OptionA.png'),
+  optionB: require('../../../assets/images/icons/OptionB.png'),
+
+  plusBook: require('../../../assets/images/icons/PlusBook.png'),
+  plusCircle: require('../../../assets/images/icons/PlusCircle.png'),
+  postcard: require('../../../assets/images/icons/Postcard.png'),
+  profileImageSetting: require('../../../assets/images/icons/ProfileImageSetting.png'),
+
+  reset: require('../../../assets/images/icons/Reset.png'),
+
+  setting: require('../../../assets/images/icons/Setting.png'),
+  supportTransparent: require('../../../assets/images/icons/SupportTransparent.png'),
+
+  woman: require('../../../assets/images/icons/Woman.png'),
+  womanSmall: require('../../../assets/images/icons/WomanSmall.png'),
+};
+
+export const img = {
+  openChat1: require('../../../assets/images/img/openChat1.png'),
+  openChat2: require('../../../assets/images/img/openChat2.png'),
+  openChat3: require('../../../assets/images/img/openChat3.png'),
+  openChat4: require('../../../assets/images/img/openChat4.png'),
+  openChat5: require('../../../assets/images/img/openChat5.png'),
+  openChat6: require('../../../assets/images/img/openChat6.png'),
+  openChat7: require('../../../assets/images/img/openChat7.png'),
+  openChat8: require('../../../assets/images/img/openChat8.png'),
+
+  profileEx1: require('../../../assets/images/img/profile_ex1.png'),
+  profileEx2: require('../../../assets/images/img/profile_ex2.png'),
+  profileEx3: require('../../../assets/images/img/profile_ex3.png'),
+  profileEx4: require('../../../assets/images/img/profile_ex4.png'),
+  profileEx5: require('../../../assets/images/img/profile_ex5.png'),
+  profileEx6: require('../../../assets/images/img/profile_ex6.png'),
+};
