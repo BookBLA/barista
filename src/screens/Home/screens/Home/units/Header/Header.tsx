cd65503ebd91819155226e1afd6c6e540de7f999
eggmun98 <eggmun98@gmail.com>
2024-04-07 14:37:50 +0900

    feat: memberpostcard api 연동

--- src/screens/Home/screens/Home/units/Header/Header.tsx
+++ src/screens/Home/screens/Home/units/Header/Header.tsx
@@ -2,10 +2,12 @@ import * as S from './Header.styles';
 import LogoDarkBg from '../../../../../../../assets/images/logos/logoDarkBg.png';
 import useMovePage from '../../../../../../commons/hooks/useMovePage';
 import { icons } from '../../../../../../commons/utils/variablesImages';
+import useFetchMemberPostcard from '../../../../../../commons/hooks/useMemberPostcar';
 
 // Todo: 공통헤더로 분리예정
 const Header = () => {
   const { movePage } = useMovePage();
+  const { memberPostcard } = useFetchMemberPostcard();
 
   return (
     <S.HeaderWrapper>
@@ -17,7 +19,7 @@ const Header = () => {
         <S.IconButton onPress={movePage('product')}>
           <S.IconImage source={icons.postcard} />
         </S.IconButton>
-        <S.IconText>3</S.IconText>
+        <S.IconText>{memberPostcard}</S.IconText>
         <S.IconButton onPress={movePage('notice')}>
           <S.IconImage source={icons.bellOn} />
         </S.IconButton>
