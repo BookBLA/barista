352d6094053c30a6e285bd7485b14b498c0c89df
eggmun98 <eggmun98@gmail.com>
2024-04-28 17:50:26 +0900

    feat: agreemnt변수 공용으로 변경

--- src/screens/TermsOfService/TermsOfService.tsx
+++ src/screens/TermsOfService/TermsOfService.tsx
@@ -7,6 +7,7 @@ import Checkbox from 'expo-checkbox';
 import React, { useEffect, useState } from 'react';
 import nextArrow from '../../../assets/images/icons/NextArrow.png';
 import { useAgreementStore } from '../../commons/store/useAgreement';
+import { agreementTitles, agreementUrls } from '../../commons/contents/agreement/agreementUrls';
 
 const TermsOfService = () => {
   useEffect(() => {
@@ -14,26 +15,12 @@ const TermsOfService = () => {
   });
   const { movePage } = useMovePage();
   const { updateAgreement, agreementInfo } = useAgreementStore();
-  const agreementTitles = [
-    '약관 전체 동의',
-    '만 18세 이상입니다 (필수)',
-    '서비스 이용약관 동의 (필수)',
-    '개인정보 수집 및 이용 동의 (필수)',
-    '개인정보 제3자 제공 동의 (필수)',
-    '민감정보 수집 및 이용 동의 (필수)',
-    '마케팅 정보 수신 동의 (선택)',
-  ];
-  const agreementUrls = [
-    'https://rust-sprite-73f.notion.site/9f79f7b3c5a24e01b4d592a8be8ba5b7',
-    'https://rust-sprite-73f.notion.site/8048e82a8d964df3ac5298ecb03f9114',
-    'https://rust-sprite-73f.notion.site/3-c7de210a0d9f4500844ff813d73451ea',
-    'https://rust-sprite-73f.notion.site/442c4ef7242f42dc935849e159e8f65d',
-  ];
 
   const handleAgreementLinkPress = (index: number) => {
     const url = agreementUrls[index - 2]; // index는 1부터 시작하므로 1을 빼줘야 해당 인덱스의 주소에 접근 가능
     Linking.openURL(url);
   };
+  const qqq = 10;
 
   const [isChecked, setIsChecked] = useState(Array(agreementTitles.length).fill(false)); // Initialize an array of checkbox states
   const [isActive, setIsActive] = useState<boolean>(false);
