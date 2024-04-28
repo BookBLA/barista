4a8130be334387b9e03561076d7e850d3b6fdaeb
eggmun98 <eggmun98@gmail.com>
2024-04-28 17:50:57 +0900

    feat: useLinkingOpen 훅 구현

---
+++ src/commons/hooks/useLinkingOpen.ts
@@ -0,0 +1,7 @@
+import { Linking } from 'react-native';
+export const useLinkingOpen = () => {
+  const handleLinkPress = (url: string) => () => Linking.openURL(url);
+  return {
+    handleLinkPress,
+  };
+};
