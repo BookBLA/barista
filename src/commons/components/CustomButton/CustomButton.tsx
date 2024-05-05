dbb793653ea5a03016c00f565e5854bc98cdbae7
eggmun98 <eggmun98@gmail.com>
2024-05-05 17:12:31 +0900

    feat: 커스텀 버튼 수정

--- src/commons/components/CustomButton/CustomButton.tsx
+++ src/commons/components/CustomButton/CustomButton.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { ButtonStyled, ButtonTextStyled } from './CustomButton.styles';
-import { Text, TouchableOpacity } from 'react-native';
+import { Text } from 'react-native';
 import { IProps } from './CustomButton.types';
 
 export const CustomButton: React.FC<IProps> = ({ contents, onPress, ...rest }) => {
   return (
-    <ButtonStyled {...rest}>
-      <TouchableOpacity onPress={onPress}>
-        <ButtonTextStyled {...rest}>
-          <Text>{contents}</Text>
-        </ButtonTextStyled>
-      </TouchableOpacity>
+    <ButtonStyled {...rest} onPress={onPress}>
+      <ButtonTextStyled {...rest}>
+        <Text>{contents}</Text>
+      </ButtonTextStyled>
     </ButtonStyled>
   );
 };
