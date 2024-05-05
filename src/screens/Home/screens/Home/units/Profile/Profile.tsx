3481d07d294deb4abe61674c619ce6df0641f36b
eggmun98 <eggmun98@gmail.com>
2024-05-05 19:43:27 +0900

    feat: Profile 컴포넌트 책 이름 12자 넘어갈 시 ...처리

--- src/screens/Home/screens/Home/units/Profile/Profile.tsx
+++ src/screens/Home/screens/Home/units/Profile/Profile.tsx
@@ -8,11 +8,12 @@ import { icons } from '../../../../../../commons/utils/variablesImages';
 const Profile = ({ item }: { item: IDdata }) => {
   const { movePage } = useMovePage();
   const modifiedName = item?.memberName ? `${item.memberName.charAt(0)}0${item.memberName.slice(2)}` : '이름';
+  const bookName = item?.bookName ? item.bookName.substring(0, 12).padEnd(15, '...') : '책';
 
   return (
     <S.ProfileWrapper onPress={movePage('OtherLibrary', { userId: item?.memberId, isYourLibrary: true })}>
       <S.BookImage source={item.bookImageUrl ? { uri: item.bookImageUrl } : icons.bookCover} />
-      <CustomText size="12px">{item?.bookName ?? '책'}</CustomText>
+      <CustomText size="12px">{bookName}</CustomText>
       <CustomText size="10px">{`${modifiedName} (${item?.memberAge ?? '0'}살)`}</CustomText>
       <CustomText size="10px" color={colors.textGray}>
         {item?.memberSchoolName ?? '?? 대학교'}
