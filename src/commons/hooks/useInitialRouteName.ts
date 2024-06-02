9e23a61521bc87f5539cb699ae1906dcdfa1af03
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-06-02 18:35:08 +0900

    chore: memberStatus 내부 수정

--- src/commons/hooks/useInitialRouteName.ts
+++ src/commons/hooks/useInitialRouteName.ts
@@ -5,41 +5,42 @@ type TRouteMap = {
 };
 
 export const useInitialRouteName = () => {
-  let memberStatus = useMemberStore((state) => state.memberInfo.memberStatus);
+  const memberStatus = useMemberStore((state) => state.memberInfo.memberStatus);
 
   const getInitialRouteName = (memberStatusParam?: string) => {
-    if (memberStatusParam) {
-      switch (memberStatusParam) {
-        case 'PROFILE':
-          memberStatus = 'p';
-          break;
-        case 'APPROVAL':
-          memberStatus = 'a';
-          break;
-        case 'STYLE':
-          memberStatus = 's';
-          break;
-        case 'BOOK':
-          memberStatus = 'bo';
-          break;
-        case 'MATCHING_DISABLED':
-        case 'COMPLETED':
-          memberStatus = 'c';
-          break;
-        default:
-          return 'loginStack';
-      }
-    }
+    // if (memberStatusParam) {
+    //   switch (memberStatusParam) {
+    //     case 'PROFILE':
+    //       memberStatus = 'terms';
+    //       break;
+    //     case 'APPROVAL':
+    //       memberStatus = 'a';
+    //       break;
+    //     case 'STYLE':
+    //       memberStatus = 's';
+    //       break;
+    //     case 'BOOK':
+    //       memberStatus = 'bo';
+    //       break;
+    //     case 'MATCHING_DISABLED':
+    //     case 'COMPLETED':
+    //       memberStatus = 'c';
+    //       break;
+    //     default:
+    //       return 'loginStack';
+    //   }
+    // }
     // console.log('memberStatus', memberStatus); // p, a, s, bo, c, undefined (default: loginStack
     const routeMap: TRouteMap = {
-      p: 'termsOfService',
-      a: 'waitConfirm',
-      s: 'initStyleStack',
-      bo: 'initBookStack',
-      c: 'tapScreens',
+      PROFILE: 'termsOfService',
+      APPROVAL: 'waitConfirm',
+      STYLE: 'initStyleStack',
+      BOOK: 'initBookStack',
+      COMPLETED: 'tapScreens',
+      MATCHING_DISABLED: 'tapScreens',
     };
     // console.log('routeMap', routeMap[memberStatus]);
-    return routeMap[memberStatus];
+    return routeMap[memberStatusParam ?? memberStatus] || 'loginStack';
   };
 
   return getInitialRouteName;
