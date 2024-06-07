132f131949c0788dfceb5070c214d463fd1fa150
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-06-07 16:01:50 +0900

    fix: 이메일 인증 오류 수정

--- src/screens/InitStyle/units/Mbti.tsx
+++ src/screens/InitStyle/units/Mbti.tsx
@@ -1,5 +1,5 @@
 import { colors } from '../../../commons/styles/variablesStyles';
-import Example02 from '../../Example02/Example02';
+import MbtiComponent from '../../../commons/components/MbtiComponent/MbtiComonent';
 import * as S from '../../InitUserInfo/InitUserInfo.styles';
 import { TouchableOpacity, View, Image, Text } from 'react-native';
 import nextButton from '../../../../assets/images/buttons/nextButton.png';
@@ -31,7 +31,7 @@ const Mbti = () => {
         <S.ContentStyled style={{ marginBottom: 8 }}>MBTI를 알려주세요.</S.ContentStyled>
         <Text style={{ color: colors.textGray2, fontFamily: 'fontMedium', fontSize: 14 }}>4가지 모두 골라주세요.</Text>
       </View>
-      <Example02 mbti={mbti} setMbti={setMbti} />
+      <MbtiComponent mbti={mbti} setMbti={setMbti} />
       <S.ButtonArea style={{ justifyContent: 'flex-end' }}>
         <S.MoveButton onPress={() => nextPage()}>
           <Image source={nextButton} />
