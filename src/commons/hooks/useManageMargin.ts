26402e573256f19e52a924d696aa860aa50fa7c0
eggmun98 <eggmun98@gmail.com>
2024-06-08 15:31:53 +0900

    feat: useManageMargin훅 useFocuseEffect에서 useEffect로 변경

--- src/commons/hooks/useManageMargin.ts
+++ src/commons/hooks/useManageMargin.ts
@@ -1,19 +1,15 @@
-import { useCallback } from 'react';
+import { useEffect } from 'react';
 import { useHasMargin } from '../store/useHasMargin';
-import { useFocusEffect } from '@react-navigation/native';
 
-// todo: 개선예정
 const useManageMargin = () => {
   const { setHasMargin, hasMargin } = useHasMargin();
 
-  useFocusEffect(
-    useCallback(() => {
-      setHasMargin(false);
-      return () => {
-        setHasMargin(true);
-      };
-    }, [hasMargin]),
-  );
+  useEffect(() => {
+    setHasMargin(false);
+    return () => {
+      setHasMargin(true);
+    };
+  }, [hasMargin]);
 };
 
 export default useManageMargin;
