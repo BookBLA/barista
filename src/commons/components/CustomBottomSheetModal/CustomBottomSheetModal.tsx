9bffa74f9cdb6bfa832ecfd84f88b8765a2ac049
eggmun98 <eggmun98@gmail.com>
2024-06-09 00:02:40 +0900

    fix: 안드로이드 - 뒤로가기 오류 수정

--- src/commons/components/CustomBottomSheetModal/CustomBottomSheetModal.tsx
+++ src/commons/components/CustomBottomSheetModal/CustomBottomSheetModal.tsx
@@ -1,14 +1,46 @@
-import React, { forwardRef, useCallback } from 'react';
+import React, { forwardRef, useCallback, useEffect, useImperativeHandle, useRef } from 'react';
 import { BottomSheetBackdrop, BottomSheetModal } from '@gorhom/bottom-sheet';
 import { IProps } from './CustomBottomSheetModal.types';
+import { BackHandler } from 'react-native';
 
 const CustomBottomSheetModal = forwardRef<BottomSheetModal, IProps>(({ children, snapPoints, index }, ref) => {
   const handleSheetChanges = useCallback((index: number) => {}, []);
+  const bottomSheetRef = useRef<BottomSheetModal>(null);
+
+  // TODO: 성진 - 타입 오류는 추후에 수정예정
+  useImperativeHandle(
+    ref,
+    () => ({
+      close: () => bottomSheetRef.current?.close(),
+      present: () => bottomSheetRef.current?.present(),
+      dismiss: () => bottomSheetRef.current?.dismiss(),
+      snapToIndex: (index: number) => bottomSheetRef.current?.snapToIndex(index),
+      snapToPosition: (position: number | string, animationConfigs?: any) =>
+        bottomSheetRef.current?.snapToPosition(position, animationConfigs),
+      expand: () => bottomSheetRef.current?.expand(),
+    }),
+    [],
+  );
 
   const renderBackdrop = useCallback(
     (props: any) => <BottomSheetBackdrop {...props} pressBehavior="close" appearsOnIndex={0} disappearsOnIndex={-1} />,
     [],
   );
+
+  useEffect(() => {
+    const backAction = () => {
+      if (bottomSheetRef.current) {
+        bottomSheetRef.current.close();
+        return true;
+      }
+      return false;
+    };
+
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+    return () => {
+      backHandler.remove();
+    };
+  }, []);
 
   return (
     <BottomSheetModal
