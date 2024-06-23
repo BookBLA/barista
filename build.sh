e1a7322678b152fe60a41a6864c66164a59b8173
eggmun98 <eggmun98@gmail.com>
2024-06-23 18:33:44 +0900

    refactor: 빌드 스크립트 수정

--- build.sh
+++ build.sh
@@ -1,51 +1,67 @@
 #!/bin/sh
 
 EXIT_SUCCESS=0
+EXIT_BUILD_SUCCES=1
 EXIT_EAS_SECRET_PUSH_FAIL=10
 EXIT_INVALID_PLATFORM=20
 EXIT_GIT_FAIL=30
+EXIT_GIT_TAG_FETCH_FAIL=31
+EXIT_GIT_TAG_FAIL=32
+EXIT_GIT_CHECKOUT_FAIL=33
 EXIT_DOPPLER_UPDATE_FALI=40
+EXIT_DOPPLER_APP_JSON_UPDATE_FALI=41
+EXIT_DOPPLER_EAS_JSON_UPDATE_FALI=42
 EXIT_FILE_REMOVE_FAIL=50
+EXIT_FILE_UPDATE_APP_JSON=51
+EXIT_FILE_UPDATE_EAS_JSON=52
+EXIT_NPM_CACHE_FIL=60
+EXIT_NPM_INSTALL_FIL=61
 
 PLATFORM=${1:-android}
-ENVIRONMENT=${2:-development}
+ENVIRONMENT=${2:-PLATFORM}
+
+echo "빌드를 진행할 환경: $ENVIRONMENT"
+echo "빌드를 진행할 플랫폼: $PLATFORM"
+
+read -p "빌드를 진행하기 전에 Git 태그를 작성했습니까? (yes/no): " confirm
+if [ "$confirm" != "yes" ]; then
+  echo "빌드를 진행하려면 Git 태그를 작성해야 합니다. 예: git tag -a 0.1.0 -m \"BookBLA - version 0.1.0\""
+  exit $EXIT_BUILD_SUCCES
+fi
 
 doppler setup --no-interactive --config $ENVIRONMENT
-
 doppler configure
 
 git fetch --tags
 if [ $? -ne 0 ]; then
   echo '태그 정보를 가져오는데 실패했습니다.'
-  exit $EXIT_GIT_FAIL
+  exit $EXIT_GIT_TAG_FETCH_FAIL
 fi
 
 LATEST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
 if [ $? -ne 0 ]; then
   echo '최신 Git 태그를 가져오는데 실패했습니다.'
-  exit $EXIT_GIT_FAIL
+  exit $EXIT_GIT_TAG_FAIL
 fi
 
 git checkout $LATEST_TAG
 if [ $? -ne 0 ]; then
   echo '태그 $LATEST_TAG로 체크아웃하는데 실패했습니다.'
-  exit $EXIT_GIT_FAIL
+  exit $EXIT_GIT_CHECKOUT_FAIL
 fi
 
-echo "태그 $LATEST_TAG로 이동했습니다."
+echo "태그 $LATEST_TAG 로 이동했습니다."
 
-# npm 캐시 지우기
 npm cache clean --force
 if [ $? -ne 0 ]; then
   echo 'npm 캐시를 지우는데 실패했습니다.'
-  exit $EXIT_GIT_FAIL
+  exit $EXIT_NPM_CACHE_FIL
 fi
 
-# npm 패키지 설치
 npm install
 if [ $? -ne 0 ]; then
   echo 'npm 패키지 설치에 실패했습니다.'
-  exit $EXIT_GIT_FAIL
+  exit $EXIT_NPM_INSTALL_FIL
 fi
 
 doppler run --command 'echo $EAS_JSON > eas.json'
@@ -64,6 +80,24 @@ doppler run --command 'echo $FIREBASE_JSON> ./bookbla-2024-firebase-adminsdk-qfs
 cat ./bookbla-2024-firebase-adminsdk-qfspu-1dcca92597.json
 echo './bookbla-2024-firebase-adminsdk-qfspu-1dcca92597.json 파일 생성했습니다.'
 
+jq --arg version "$LATEST_TAG" '.expo.version = $version' app.json > app_temp.json && mv app_temp.json app.json
+if [ $? -ne 0 ]; then
+  echo 'app.json의 버전 업데이트에 실패했습니다.'
+  exit $EXIT_FILE_UPDATE_APP_JSON
+fi
+
+echo "app.json 파일의 버전을 최신 태그로 업데이트했습니다."
+cat app.json
+
+jq --arg version "$LATEST_TAG" '.expo.version = $version' eas.json > eas_temp.json && mv eas_temp.json eas.json
+if [ $? -ne 0 ]; then
+  echo 'eas.json의 버전 업데이트에 실패했습니다.'
+  exit $EXIT_FILE_UPDATE_EAS_JSON
+fi
+
+echo "eas.json 파일의 버전을 최신 태그로 업데이트했습니다."
+cat eas.json
+
 
 # doppler run --mount .env -- eas secret:push --scope project --env-file .env --force
 # if [ $? -eq 0 ]; then
@@ -77,18 +111,14 @@ case $PLATFORM in
   all)
     echo '모든 플랫폼에 대해 빌드를 진행합니다.'
     eas build --platform all --profile $ENVIRONMENT 
-    eas submit --platform ios
-    eas submit --platform android
     ;;
   android)
     echo '안드로이드 플랫폼에 대해 빌드를 진행합니다.'
     eas build --platform android --profile $ENVIRONMENT 
-    eas submit --platform android
     ;;
   ios)
     echo 'iOS 플랫폼에 대해 빌드를 진행합니다.'
     eas build --platform ios --profile $ENVIRONMENT 
-    eas submit --platform ios
     ;;
   *)
     echo '유효하지 않은 플랫폼입니다. 'all', 'android', 'ios' 중 하나를 선택하세요.'
@@ -96,27 +126,39 @@ case $PLATFORM in
     ;;
 esac
 
-
-app.json의 version 업데이트
-jq --arg version "$LATEST_TAG" '.expo.version = $version' app.json > app_temp.json && mv app_temp.json app.json
-if [ $? -ne 0 ]; then
-  echo 'app.json의 버전 업데이트에 실패했습니다.'
-  exit $EXIT_GIT_FAIL
-fi
-
-echo "app.json 파일의 버전을 최신 태그로 업데이트했습니다."
-cat app.json
-
-
 doppler secrets set APP_JSON="$(cat app.json)"
 if [ $? -eq 0 ]; then
   echo 'app.json을 성공적으로 Doppler에 업데이트했습니다.'
 else
   echo 'app.json을 Doppler에 업데이트하는데 실패했습니다.'
-  exit $EXIT_DOPPLER_UPDATE_FALI
+  exit $EXIT_DOPPLER_APP_JSON_UPDATE_FALI
 fi
 
-# eas 빌드 시 실패할 경우 어떻게 할건지?
+doppler secrets set EAS_JSON="$(cat eas.json)"
+if [ $? -eq 0 ]; then
+  echo 'eas.json을 성공적으로 Doppler에 업데이트했습니다.'
+else
+  echo 'eas.json을 Doppler에 업데이트하는데 실패했습니다.'
+  exit $EXIT_DOPPLER_EAS_JSON_UPDATE_FALI
+fi
+
+
+case $PLATFORM in
+  all)
+    echo '모든 플랫폼에 대해 제출을 진행합니다.'
+    eas submit --platform ios
+    eas submit --platform android
+    ;;
+  android)
+    echo '안드로이드 플랫폼에 대해 제출을 진행합니다.'
+    eas submit --platform android
+    ;;
+  ios)
+    echo 'iOS 플랫폼에 대해 제출을 진행합니다.'
+    eas submit --platform ios
+    ;;
+esac
+
 rm eas.json google-services.json app.json ./bookbla-2024-firebase-adminsdk-qfspu-1dcca92597.json
 if [ $? -eq 0 ]; then
   echo '생성된 json파일들을 성공적으로 삭제했습니다.'
