c7fe3f6a00c9bd606d373711d96bbd6a0fb5c957
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-06-25 23:39:05 +0900

    style: placeholder(부적절하거나 불쾌감을 줄 수 있는 컨텐츠) 추가

--- src/screens/InitUserInfo/units/components/RejectedInfo/OpenChat.tsx
+++ src/screens/InitUserInfo/units/components/RejectedInfo/OpenChat.tsx
@@ -43,6 +43,7 @@ const OpenChat = () => {
               링크만 입력해 주셔야 채팅을 보낼 수 있습니다.
             </Text>
             <OpenChatTextFiledStyled
+              placeholder="부적절하거나 불쾌감을 줄 수 있는 컨텐츠는 제재를 받을 수 있습니다."
               defaultValue={userInfo.openKakaoRoomUrl}
               onChangeText={(text: string) => updateUserInfo({ openKakaoRoomUrl: text })}
               style={{
