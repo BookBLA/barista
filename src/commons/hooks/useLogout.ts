08138072374c57b49dd629b4f72a911a89d7f216
eggmun98 <eggmun98@gmail.com>
2024-07-14 19:31:36 +0900

    update: useLogout훅 로직 수정

--- src/commons/hooks/useLogout.ts
+++ src/commons/hooks/useLogout.ts
@@ -1,11 +1,18 @@
 import useAuthStore from '../store/useAuthStore';
+import { useStyleStore } from '../store/useStyle';
 import useToastStore from '../store/useToastStore';
+import { useUserStore } from '../store/useUserinfo';
 
-export const useLogout = () => {
+export const useLogout = (logoutToggle: () => void) => {
   const removeToken = useAuthStore((state) => state.removeToken);
   const showToast = useToastStore((state) => state.showToast);
+  const resetUserInfo = useUserStore((state) => state.resetUserInfo);
+  const resetStyleInfo = useStyleStore((state) => state.resetStyleInfo);
 
   const onClickLogout = () => {
+    resetUserInfo();
+    resetStyleInfo();
+    logoutToggle();
     showToast({
       content: '로그아웃 하였습니다.',
     });
