34d5d0622ed7d341ee154c9ce87994a32a5a9b59
eggmun98 <eggmun98@gmail.com>
2024-07-14 19:32:48 +0900

    feat: 커스텀 스위치 구현

---
+++ src/commons/components/CustomSwitch/CustomSwitch.tsx
@@ -0,0 +1,24 @@
+import { Switch } from 'react-native-switch';
+import { colors } from '../../styles/variablesStyles';
+import { IProps } from './CustomSwitch.types';
+
+export const CustomSwitch = ({ value, onValueChange }: IProps) => {
+  return (
+    <Switch
+      value={value}
+      onValueChange={onValueChange}
+      circleSize={16}
+      barHeight={20}
+      circleBorderWidth={0}
+      backgroundActive={colors.primary}
+      backgroundInactive={colors.buttonAuthToggle}
+      circleActiveColor="#fff"
+      circleInActiveColor="#fff"
+      renderActiveText={false}
+      renderInActiveText={false}
+      switchLeftPx={3}
+      switchRightPx={3}
+      switchWidthMultiplier={2}
+    />
+  );
+};
