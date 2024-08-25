b775d0592877c8321da12d6478c05b40d3626021
banggeunho <panggeunho@gmail.com>
2024-08-25 15:08:39 +0900

    feat: 우편함 탭 > 상단 탭 변경, 엽서 비어있는 경우 변경, 책갈피 이미지 변경

--- src/screens/Matching/Matching.tsx
+++ src/screens/Matching/Matching.tsx
@@ -6,21 +6,23 @@ import { useUserStore } from '@commons/store/members/userinfo/useUserinfo';
 import { icons } from '@commons/utils/ui/variablesImages/variablesImages';
 import { IconButton, IconImage } from '@screens/Home/screens/Home/units/Header/Header.styles';
 import { useFetchReceivePostcard } from '@screens/Home/screens/Matching/hooks/useFetchReceivePostcard';
-import { useFetchSendPostcard } from '@screens/Home/screens/Matching/hooks/useFetchSendPostcard';
 import React, { useRef, useState } from 'react';
 import { FlatList, TouchableOpacity, View } from 'react-native';
 import * as S from './Matching.styles';
 import { EmptyPostcard } from './Postcard/EmptyPostcard';
 import { EType } from './Postcard/EmptyPostcard.types';
 import { ReceivePostcard } from './Postcard/Receive/ReceivePostcard';
-import { SendPostcard } from './Postcard/Send/SendPostcard';
+import useHeaderControl from '@commons/hooks/ui/headerControl/useHeaderControl';
 
 const Matching = () => {
   useScreenLogger();
+  useHeaderControl({
+    title: '우편함',
+    left: false,
+  });
   const { memberPostcard } = useFetchMemberPostcard();
   const [isReceivedPostcard, setIsReceivedPostcard] = useState<boolean>(true);
   const receivedPostcards = useFetchReceivePostcard(isReceivedPostcard);
-  const sendPostcards = useFetchSendPostcard(isReceivedPostcard);
   const [showButton, setShowButton] = useState<boolean>(false);
   const flatListRef = useRef<FlatList>(null);
   const { movePage } = useMovePage();
@@ -44,95 +46,36 @@ const Matching = () => {
 
   return (
     <S.Wrapper>
-      <S.ViewStyled>
-        <View style={{ flex: 1, height: '80%' }}>
-          <S.PressableStyled
-            onPress={() => {
-              setIsReceivedPostcard(true);
-              setShowButton(false);
-            }}
-            style={({ pressed }: any) => [
-              {
-                backgroundColor: pressed ? 'lightgrey' : isReceivedPostcard ? 'white' : 'transparent',
-              },
-              S.PressableStyled,
-            ]}
-          >
-            <S.PressableTextStyled>받은 엽서</S.PressableTextStyled>
-          </S.PressableStyled>
-        </View>
-        <View style={{ flex: 1, height: '80%' }}>
-          <S.PressableStyled
-            onPress={() => {
-              setIsReceivedPostcard(false);
-              setShowButton(false);
-            }}
-            style={({ pressed }: any) => [
-              {
-                backgroundColor: pressed ? 'lightgrey' : isReceivedPostcard ? 'transparent' : 'white',
-              },
-              S.PressableStyled,
-            ]}
-          >
-            <S.PressableTextStyled>내가 보낸 엽서</S.PressableTextStyled>
-          </S.PressableStyled>
-        </View>
-      </S.ViewStyled>
       <S.ListWrapper>
-        {isReceivedPostcard ? (
-          <>
-            <S.InfoViewStyled>
-              <S.InfoTextStyled>받은 엽서 확인 시 소지한 엽서 1개가 소모 됩니다</S.InfoTextStyled>
-              <S.postcardCountViewStyled>
-                <IconButton onPress={movePage('product')}>
-                  <IconImage source={icons.postcard} />
-                </IconButton>
-                <S.postcardCountTextStyled>{memberPostcard}</S.postcardCountTextStyled>
-              </S.postcardCountViewStyled>
-            </S.InfoViewStyled>
-            {receivedPostcards.length === 0 && <EmptyPostcard type={EType.RECEIVE} />}
-            {receivedPostcards.length > 0 && (
-              <FlatList
-                style={{ paddingLeft: 16, paddingRight: 16, marginTop: 10 }}
-                ref={flatListRef}
-                data={receivedPostcards}
-                renderItem={({ item, index }) => (
-                  <View style={{ height: '100%', width: '50%' }}>
-                    <S.receivedPostcardViewStyled style={S.styles.PostcardShadow} index={index}>
-                      <ReceivePostcard key={item.postcardId} {...item} />
-                    </S.receivedPostcardViewStyled>
-                  </View>
-                )}
-                keyExtractor={(item) => item.postcardId.toString()}
-                numColumns={2}
-                onScroll={handleScroll}
-                alwaysBounceVertical={false}
-                ListFooterComponent={<View style={{ height: 100 }} />}
-                overScrollMode="never"
-              />
+        <S.InfoViewStyled>
+          <S.InfoTextStyled>받은 엽서 확인 시 소지한 엽서 5개가 소모 됩니다</S.InfoTextStyled>
+          <S.postcardCountViewStyled>
+            <IconButton onPress={movePage('product')}>
+              <IconImage source={icons.postcard} />
+            </IconButton>
+            <S.postcardCountTextStyled>{memberPostcard}</S.postcardCountTextStyled>
+          </S.postcardCountViewStyled>
+        </S.InfoViewStyled>
+        {receivedPostcards.length === 0 && <EmptyPostcard type={EType.RECEIVE} />}
+        {receivedPostcards.length > 0 && (
+          <FlatList
+            style={{ paddingLeft: 16, paddingRight: 16, marginTop: 10 }}
+            ref={flatListRef}
+            data={receivedPostcards}
+            renderItem={({ item, index }) => (
+              <View style={{ height: '100%', width: '50%' }}>
+                <S.receivedPostcardViewStyled style={S.styles.PostcardShadow} index={index}>
+                  <ReceivePostcard key={item.postcardId} {...item} />
+                </S.receivedPostcardViewStyled>
+              </View>
             )}
-          </>
-        ) : (
-          <>
-            {sendPostcards.length === 0 && <EmptyPostcard type={EType.SEND} />}
-            {sendPostcards.length > 0 && (
-              <FlatList
-                style={{ paddingLeft: 16, paddingRight: 16 }}
-                ref={flatListRef}
-                data={sendPostcards}
-                renderItem={({ item, index }) => (
-                  <S.sendPostcardViewStyled>
-                    <SendPostcard key={item.postcardId} {...item} />
-                  </S.sendPostcardViewStyled>
-                )}
-                keyExtractor={(item, index) => index.toString()}
-                onScroll={handleScroll}
-                alwaysBounceVertical={false}
-                ListFooterComponent={<View style={{ height: 100 }} />}
-                overScrollMode="never"
-              />
-            )}
-          </>
+            keyExtractor={(item) => item.postcardId.toString()}
+            numColumns={2}
+            onScroll={handleScroll}
+            alwaysBounceVertical={false}
+            ListFooterComponent={<View style={{ height: 100 }} />}
+            overScrollMode="never"
+          />
         )}
       </S.ListWrapper>
       {showButton && (
