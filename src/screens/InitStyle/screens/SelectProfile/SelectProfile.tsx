0037c2003bbc85c6576b05172d635aeb3dbac444
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-08-26 15:43:31 +0900

    feat: 기본 프로필 설정 페이지 제작

---
+++ src/screens/InitStyle/screens/SelectProfile/SelectProfile.tsx
@@ -0,0 +1,128 @@
+import nextButton from '@assets/images/buttons/nextButton.png';
+import prevButton from '@assets/images/buttons/prevButton.png';
+import { getProfileImageType } from '@commons/api/members/profile/memberProfile.api';
+import { postMemberStyleApi } from '@commons/api/members/styles/memberStyle.api';
+import useMovePage from '@commons/hooks/navigations/movePage/useMovePage';
+import useHeaderControl from '@commons/hooks/ui/headerControl/useHeaderControl';
+import useManageMargin from '@commons/hooks/ui/manageMargin/useManageMargin';
+import { useStyleStore } from '@commons/store/members/style/useStyle';
+import useToastStore from '@commons/store/ui/toast/useToastStore';
+import { ProfileImageResponse } from '@commons/types/openapiGenerator';
+import * as S from '@screens/InitUserInfo/InitUserInfo.styles';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useEffect, useState } from 'react';
+import { Image, TouchableOpacity, View } from 'react-native';
+
+const SelectProfile = () => {
+  const showToast = useToastStore((state) => state.showToast);
+  useHeaderControl({
+    title: '스타일',
+    left: false,
+  });
+  useManageMargin();
+  const { updateStyleInfo, styleInfo, resetStyleInfo } = useStyleStore();
+  const { movePage, handleReset } = useMovePage();
+  const [profile, setProfile] = useState(styleInfo.profileImageTypeId);
+  const [profileList, setProfileList] = useState<{ profileImageId: number; profileImageUrl: string }[]>([]);
+
+  useEffect(() => {
+    callGetProfileImage();
+  }, []);
+
+  const nextPage = async () => {
+    await callPostStyleApi();
+  };
+
+  const callGetProfileImage = async () => {
+    try {
+      const response = await getProfileImageType();
+      const profiles = response.result.profileImageResponseTypes.map((item: ProfileImageResponse) => ({
+        profileImageId: item.profileImageTypeId,
+        profileImageUrl: item.profileImageUrl,
+      }));
+      styleInfo.profileImageTypeId === 0 && setProfile(profiles[0].profileImageId);
+      updateStyleInfo('profileImageTypeId', profiles[0].profileImageId);
+      setProfileList(profiles);
+    } catch (error) {
+      console.log('ERROR) GetProfileImageType', error);
+    }
+  };
+
+  const callPostStyleApi = async () => {
+    try {
+      console.log('styleInfo', styleInfo);
+      const response = await postMemberStyleApi(styleInfo);
+      console.log('postMemberStyleApi', response);
+      handleReset('initBookStack');
+      resetStyleInfo();
+    } catch (error) {
+      console.log('ERROR) postMemberStyleApi', error);
+      showToast({
+        content: error.response.data.message,
+      });
+    }
+  };
+
+  return (
+    <S.Wrapper>
+      <View style={{ width: '100%', alignItems: 'center', marginTop: '34%' }}>
+        <S.ContentStyled style={{ marginBottom: 38, marginTop: 30 }}>프로필 사진을 선택해 주세요</S.ContentStyled>
+
+        <Image
+          source={{ uri: profileList.find((item) => item.profileImageId === profile)?.profileImageUrl ?? '' }}
+          style={{ width: 161, height: 161, marginBottom: 40 }}
+        />
+
+        <S.RowStyled style={{ alignItems: 'center', width: '73%' }}>
+          {profileList.map((item) =>
+            item.profileImageId === profile ? (
+              <LinearGradient
+                key={item.profileImageId}
+                style={{
+                  width: 62,
+                  height: 62,
+                  borderRadius: 100,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+                colors={['#5B247A', '#1BCEDF']}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
+              >
+                <TouchableOpacity
+                  key={item.profileImageId}
+                  onPress={() => {
+                    setProfile(item.profileImageId);
+                    updateStyleInfo('profileImageTypeId', item.profileImageId);
+                  }}
+                  style={{ borderColor: 'white', borderWidth: 3.5, borderRadius: 100 }}
+                >
+                  <Image source={{ uri: item.profileImageUrl }} style={{ width: 53, height: 53 }} />
+                </TouchableOpacity>
+              </LinearGradient>
+            ) : (
+              <TouchableOpacity
+                key={item.profileImageId}
+                onPress={() => {
+                  setProfile(item.profileImageId);
+                  updateStyleInfo('profileImageTypeId', item.profileImageId);
+                }}
+              >
+                <Image source={{ uri: item.profileImageUrl }} style={{ width: 53, height: 53 }} />
+              </TouchableOpacity>
+            ),
+          )}
+        </S.RowStyled>
+      </View>
+      <S.ButtonArea>
+        <S.MoveButton onPress={movePage()}>
+          <Image source={prevButton} />
+        </S.MoveButton>
+        <S.MoveButton onPress={nextPage}>
+          <Image source={nextButton} />
+        </S.MoveButton>
+      </S.ButtonArea>
+    </S.Wrapper>
+  );
+};
+export default SelectProfile;
