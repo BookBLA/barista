7fdc1792f26d48dea59b421fc558ff8f9fae3166
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-09-05 00:30:54 +0900

    feat: initRouteName에 schoolStatus 추가(미완)

--- src/commons/hooks/navigations/initialRouteName/useInitialRouteName.ts
+++ src/commons/hooks/navigations/initialRouteName/useInitialRouteName.ts
@@ -8,6 +8,13 @@ export const useInitialRouteName = () => {
   const memberStatus = useMemberStore((state) => state.memberInfo.memberStatus);
 
   const getInitialRouteName = (memberStatusParam?: string) => {
+    // let schoolStatus: string | undefined;
+
+    // if (memberStatus === 'STYLE') {
+    //   const response = await getMemberStatusesApi();
+    //   schoolStatus = response.result?.schoolStatus;
+    // }
+
     const routeMap: TRouteMap = {
       PROFILE: 'initUserinfoStack',
       // APPROVAL: 'waitConfirm',
@@ -17,14 +24,13 @@ export const useInitialRouteName = () => {
       MATCHING_DISABLED: 'tapScreens',
       DEFAULT: 'loginStack',
     };
+
+    // if (schoolStatus === 'CLOSED' && memberStatus === 'STYLE') {
+    //   return 'inviteFriends';
+    // } else return routeMap[memberStatusParam ?? memberStatus ?? 'DEFAULT'];
+
     return routeMap[memberStatusParam ?? memberStatus ?? 'DEFAULT'];
   };
 
   return getInitialRouteName;
-  // const response = await getMemberStatusesApi();
-  // const schoolStatus = response.result?.schoolStatus;
-
-  // if (schoolStatus === 'CLOSED' && memberStatus === 'STYLE') {
-  //   return 'inviteFriends';
-  // } else return getInitialRouteName;
 };
