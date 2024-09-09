9fd89ed304f060db9ea0da5c9096ef77eed46a79
Hangyeol-SSeo <dongha313@gmail.com>
2024-09-09 18:12:17 +0900

    chore: package 의존성 충돌 해결

--- src/screens/Home/screens/Home/units/MemberCard/MemberCard.styles.ts
+++ src/screens/Home/screens/Home/units/MemberCard/MemberCard.styles.ts
@@ -21,6 +21,6 @@ export const SendButton = styled.TouchableOpacity`
 
   width: 100%;
   height: 44px;
-  border-radius: 0px 0px 10px 10px;
+  border-radius: 0 0 10px 10px;
   background-color: ${colors.primary02};
 `;
