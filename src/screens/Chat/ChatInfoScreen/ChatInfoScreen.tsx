3cd9586b3368691deb98d0b0eed810f244e9cac0
Ryugi62 <xorjf1027@naer.com>
2024-09-14 19:29:55 +0900

    feat: Update ChatInfoScreen UI with new colors
    This commit updates the UI of the ChatInfoScreen component by changing the color of the back button, options icons, and chevron icons to #1D2E61. This change improves the visual consistency and enhances the overall user experience.

--- src/screens/Chat/ChatInfoScreen/ChatInfoScreen.tsx
+++ src/screens/Chat/ChatInfoScreen/ChatInfoScreen.tsx
@@ -24,7 +24,7 @@ const ChatInfoScreen: React.FC<ChatInfoScreenProps> = ({ route }) => {
     <View style={styles.container}>
       <View style={styles.header}>
         <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
-          <Ionicons name="chevron-back" size={24} color="black" />
+          <Ionicons name="chevron-back" size={24} color="#1D2E61" />
         </TouchableOpacity>
         <Text style={styles.headerTitle}>채팅방 정보</Text>
       </View>
@@ -36,21 +36,26 @@ const ChatInfoScreen: React.FC<ChatInfoScreenProps> = ({ route }) => {
 
       <View style={styles.optionsSection}>
         <View style={styles.optionItem}>
-          <Ionicons name="notifications-outline" size={24} color="black" />
+          <Ionicons name="notifications-outline" size={24} color="#1D2E61" />
           <Text style={styles.optionText}>알림</Text>
-          <Switch style={styles.switch} onValueChange={toggleSwitch} value={isNotificationEnabled} />
+          <Switch
+            style={styles.switch}
+            onValueChange={toggleSwitch}
+            value={isNotificationEnabled}
+            trackColor={{ false: '#767577', true: '#1D2E61' }}
+          />
         </View>
 
         <TouchableOpacity style={styles.optionItem}>
-          <Ionicons name="book-outline" size={24} color="black" />
+          <Ionicons name="book-outline" size={24} color="#1D2E61" />
           <Text style={styles.optionText}>서재 구경하기</Text>
-          <Ionicons name="chevron-forward" size={24} color="black" />
+          <Ionicons name="chevron-forward" size={24} color="#1D2E61" />
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.optionItem}>
-          <Ionicons name="alert-circle-outline" size={24} color="black" />
+          <Ionicons name="alert-circle-outline" size={24} color="#1D2E61" />
           <Text style={styles.optionText}>신고하기</Text>
-          <Ionicons name="chevron-forward" size={24} color="black" />
+          <Ionicons name="chevron-forward" size={24} color="#1D2E61" />
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.optionItem}>
