7d0bce35ef7d8a50bb36794c92c6047c42f26144
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-09-15 13:53:16 +0900

    feat: paymentApi 연결

--- src/screens/Home/screens/Product/components/ProductListContent.tsx
+++ src/screens/Home/screens/Product/components/ProductListContent.tsx
@@ -1,12 +1,14 @@
+import { postPaymentApi } from '@commons/api/payment/payment.api';
 import { CustomButton } from '@commons/components/Inputs/CustomButton/CustomButton';
 import { CustomText } from '@commons/components/Utils/TextComponents/CustomText/CustomText';
 import { useEffect } from 'react';
-import { Image, View } from 'react-native';
+import { Image, Platform, View } from 'react-native';
 import { ProductPurchase, requestPurchase, useIAP } from 'react-native-iap';
 import productMask from '../../../../../../assets/images/icons/ProductMask.png';
 import { CustomGradientButton } from '../../../../../commons/components/Inputs/CustomGradientButton/CustomGradientButton';
 import { colors } from '../../../../../commons/styles/variablesStyles';
-import { ProductProps } from './ProductList.types';
+import * as S from '../Product.styles';
+import { ProductProps } from '../Product.types';
 
 const ProductListContent: React.FC<ProductProps> = ({ props, index, admobCount }) => {
   const { title, krwPrice, localizedPrice, discount, originalPrice, productId } = props;
@@ -18,15 +20,31 @@ const ProductListContent: React.FC<ProductProps> = ({ props, index, admobCount }
         sku,
         andDangerouslyFinishTransactionAutomaticallyIOS: false, // requestPurchase 호출 후 자동으로 finishTransaction을 호출할지 여부
       });
+      console.log('Purchase result:', result);
+      
     } catch (err) {
-      console.error('Purchase failed:', err.code, err.message);
+      console.error('Purchase failed:', err);
     }
   };
+
+  const postPayment=async(transactionId: string)=>{
+    try{
+      let payType='apple';
+      if(Platform.OS === 'android') payType='google';
+      console.log('payType',payType);
+      const response =await postPaymentApi(payType, transactionId);
+      console.log('postPaymentApi',response);
+    }catch(err){
+      console.error(err);
+    }
+  };
+
   useEffect(() => {
     const checkCurrentPurchase = async (purchase: ProductPurchase): Promise<void> => {
       if (purchase) {
         try {
-          console.log(JSON.stringify(purchase, null, 2));
+          await postPayment(purchase.transactionId as string);
+          console.log('checkCurrentPurchase',JSON.stringify(purchase, null, 2));
           const ackResult = await finishTransaction({ purchase });
           // andDangerouslyFinishTransactionAutomaticallyIOS 값이 false이기 때문에,
           // requestPurchase 호출 후 finishTransaction을 수동으로 호출해야만 결제가 완료된다.
@@ -43,28 +61,9 @@ const ProductListContent: React.FC<ProductProps> = ({ props, index, admobCount }
   }, [currentPurchase, finishTransaction]);
 
   return (
-    <View
-      style={{
-        width: '100%',
-        height: '100%',
-        backgroundColor: index === 0 && admobCount === 0 ? 'transparent' : 'white',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        borderRadius: 9,
-        borderColor: index === 0 ? 'transparent' : 'rgba(0, 0, 0, 0.05)',
-        borderWidth: 1,
-        opacity: index === 0 && admobCount === 0 ? 0.4 : 1,
-      }}
-    >
-      <View
-        style={{
-          flexDirection: 'row',
-          width: '50%',
-          height: '90%',
-          alignItems: 'center',
-        }}
-      >
+   
+    <S.ProductContentContainer index={index} admobCount={admobCount}>
+      <S.ProductInfoContainer>
         <Image source={productMask} style={{ width: 33, height: 42, marginRight: '10%' }} />
         <View
           style={{
@@ -101,17 +100,18 @@ const ProductListContent: React.FC<ProductProps> = ({ props, index, admobCount }
             </View>
           </View>
         </View>
-      </View>
+      </S.ProductInfoContainer>
       {index === 0 ? (
-        admobCount > 0 ? (
-          <CustomGradientButton contents={`무료 ${admobCount}/2`} onPress={() => console.log('애드몹시청')} />
+        admobCount &&
+          admobCount > 0 ? (
+            <CustomGradientButton contents={`무료 ${admobCount}/2`} onPress={() => console.log('애드몹시청')} />
+          ) : (
+            <CustomButton contents={'무료 0/2'} disabled style={{paddingTop: 9, paddingBottom: 9, paddingLeft: 18, paddingRight: 18}}/>
+          )
         ) : (
-          <CustomButton contents={'무료 0/2'} disabled />
-        )
-      ) : (
-        <CustomButton contents={'구매하기'} onPress={() => buy(productId)} />
-      )}
-    </View>
+            <CustomButton contents={'구매하기'} onPress={() => buy(productId)} padding={'9px 18px'}/>
+        )}
+    </S.ProductContentContainer>
   );
 };
 
