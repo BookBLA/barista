907a221e1e4c8c84e809136715dd56c21a3435ee
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-09-15 19:44:43 +0900

    fix: 인앱결제 api 수정

--- src/commons/api/payment/payment.api.ts
+++ src/commons/api/payment/payment.api.ts
@@ -1,5 +1,3 @@
 import { Post } from '@commons/configs/axios/http.api';
 
-export const postPaymentApi = (payType: string, receipt: string) => Post(`payments/in-app/${payType}`, {
-  "인앱결제 후 받은 영수증 정보": `${receipt}`
-});
+export const postPaymentApi = (payType: string, receipt: string) => Post(`payments/in-app/${payType}`, { transactionId: receipt });
