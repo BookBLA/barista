f29fc97e26f1bf7f939a3bff31ab91f12ef1b3ec
Ryugi62 <xorjf1027@naer.com>
2024-09-16 03:58:57 +0900

    feat: Update WebSocketClient connection URL and add isConnected flag

--- src/commons/websocket/websocketClient.ts
+++ src/commons/websocket/websocketClient.ts
@@ -1,10 +1,11 @@
 import { useErrorMessage } from '@commons/store/appStatus/errorMessage/useErrorMessage';
 import useAuthStore from '@commons/store/auth/auth/useAuthStore';
 import useToastStore from '@commons/store/ui/toast/useToastStore';
+import { Ref } from 'vue';
 
 class WebSocketClient {
   private static instance: WebSocketClient;
-  private socket: WebSocket | null = null;
+  private socket: WebSocket | null | Ref<WebSocket | null> = null;
   private url: string = 'ws://dev.bookbla.shop/api/chat/ws/connect';
   private isConnected: boolean = false; // 연결 상태 추적
 
@@ -20,7 +21,7 @@ class WebSocketClient {
   }
 
   public connect() {
-    if (this.isConnected || this.socket?.readyState === WebSocket.OPEN) {
+    if (this.isConnected || (this.socket && this.socket.value?.readyState === WebSocket.OPEN)) {
       console.debug('WebSocket is already connected');
       return;
     }
@@ -28,24 +29,30 @@ class WebSocketClient {
     const token = useAuthStore.getState().token;
     const headers = token ? { Authorization: `Bearer ${token}` } : {};
 
-    this.socket = new WebSocket(this.url, [], { headers });
+    if (this.socket && 'value' in this.socket) {
+      this.socket.value = new WebSocket(this.url, [], { headers });
+    } else {
+      this.socket = new WebSocket(this.url, [], { headers });
+    }
 
-    this.socket.onopen = () => {
+    const socket = 'value' in this.socket ? this.socket.value : this.socket;
+
+    socket.onopen = () => {
       this.isConnected = true;
       console.debug('WebSocket connection established');
     };
 
-    this.socket.onmessage = (event) => {
+    socket.onmessage = (event) => {
       console.debug('WebSocket message received:', event.data);
     };
 
-    this.socket.onerror = (error) => {
+    socket.onerror = (error) => {
       console.error('WebSocket error:', error);
       useErrorMessage.getState().setErrorMessage('WebSocket 연결 오류가 발생했습니다.');
       this.isConnected = false;
     };
 
-    this.socket.onclose = () => {
+    socket.onclose = () => {
       console.debug('WebSocket connection closed');
       this.isConnected = false;
       useErrorMessage.getState().setErrorMessage('WebSocket 연결이 종료되었습니다.');
@@ -53,9 +60,11 @@ class WebSocketClient {
   }
 
   public sendMessage(message: any) {
-    if (this.socket && this.socket.readyState === WebSocket.OPEN) {
+    const socket = 'value' in this.socket ? this.socket.value : this.socket;
+
+    if (socket && socket.readyState === WebSocket.OPEN) {
       const jsonMessage = JSON.stringify(message);
-      this.socket.send(jsonMessage);
+      socket.send(jsonMessage);
       console.debug('WebSocket message sent:', jsonMessage);
     } else {
       useToastStore.getState().showToast({ content: 'WebSocket 연결이 준비되지 않았습니다.' });
@@ -64,8 +73,10 @@ class WebSocketClient {
   }
 
   public disconnect() {
-    if (this.socket && this.socket.readyState === WebSocket.OPEN) {
-      this.socket.close();
+    const socket = 'value' in this.socket ? this.socket.value : this.socket;
+
+    if (socket && socket.readyState === WebSocket.OPEN) {
+      socket.close();
       this.isConnected = false;
       this.socket = null;
     } else {
