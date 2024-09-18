d36d7a57c4ba9047b1829c94dce1cda134c34cfd
Ryugi62 <xorjf1027@naer.com>
2024-09-18 16:57:10 +0900

    refactor: Add navigation to library screen in ChatInfoScreen

--- src/screens/Chat/ChatStack.tsx
+++ src/screens/Chat/ChatStack.tsx
@@ -1,10 +1,15 @@
 // ChatStack.tsx
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Library from '@screens/Library/Library';
 import React from 'react';
 import Chat from './Chat';
 import { ChatStackParamList } from './chat.types';
 
 const Stack = createNativeStackNavigator<ChatStackParamList>();
+const screens = [
+  { name: 'chat', component: Chat },
+  { name: 'library', component: Library },
+];
 
 const ChatStack = () => {
   return (
@@ -13,7 +18,9 @@ const ChatStack = () => {
         headerShown: false,
       }}
     >
-      <Stack.Screen name="Chat" component={Chat} />
+      {screens.map(({ name, component }) => (
+        <Stack.Screen key={name} name={name} component={component} />
+      ))}
     </Stack.Navigator>
   );
 };
