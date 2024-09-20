089c0518df50bf2186e83143a44ae35e93cf7e5d
eggmun98 <eggmun98@gmail.com>
2024-09-20 21:24:07 +0900

    feat: memberMatch 엔드포인트 api 연결 및 컴포넌트에 적용

---
+++ src/screens/Home/screens/Home/units/MemberCard/units/Profile/Profile.tsx
@@ -0,0 +1,39 @@
+import reportIcon from '@assets/images/icons/ReportIcon.png';
+import { CustomText } from '@commons/components/Utils/TextComponents/CustomText/CustomText';
+import { MemberIntroResponse } from '@commons/types/openapiGenerator';
+import { icons } from '@commons/utils/ui/variablesImages/variablesImages';
+import { EGender } from '@screens/Home/screens/Home/Home.types';
+import * as S from './Profile.styles';
+
+// TODO: 프로필 컴포넌트 공용화 예정
+const Profile = ({ memberData, handleReport }: { handleReport: () => void; memberData: MemberIntroResponse }) => {
+  const memberGender = memberData?.memberGender === EGender.MALE ? icons.man : icons.woman;
+  return (
+    <S.Wrapper>
+      <S.LeftContainer>
+        <S.LeftWrapper>
+          <S.ProfileImage source={memberData?.memberProfileImageUrl} />
+        </S.LeftWrapper>
+        <S.RightWrapper>
+          <S.InfoWrapper>
+            <CustomText>
+              {memberData?.memberName} | {memberData?.memberAge}
+            </CustomText>
+            <S.GenderWrapper>
+              <S.GenderImage source={memberGender} />
+            </S.GenderWrapper>
+          </S.InfoWrapper>
+
+          <CustomText size="12px" font="fontRegular">
+            {memberData?.memberSchoolName}
+          </CustomText>
+        </S.RightWrapper>
+      </S.LeftContainer>
+      <S.IconButton onPress={handleReport}>
+        <S.ReportIconImage source={reportIcon} />
+      </S.IconButton>
+    </S.Wrapper>
+  );
+};
+
+export default Profile;
