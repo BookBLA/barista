021f524aea8b85b2400503014e4d05ab4266fb2b
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-09-22 12:15:21 +0900

    feat: 홈에서 신고한 뒤 초대카드 보이기

--- src/screens/Home/screens/Home/Home.tsx
+++ src/screens/Home/screens/Home/Home.tsx
@@ -19,6 +19,7 @@ import React, { useEffect, useMemo, useState } from 'react';
 import Advert from './units/Advert/Advert';
 import EventCard from './units/EventCard/EventCard';
 import Header from './units/Header/Header';
+import InviteCard from './units/InviteCard/InviteCard';
 import Lock from './units/Lock/Lock';
 import MemberCard from './units/MemberCard/MemberCard';
 
@@ -30,13 +31,14 @@ const Home = () => {
   });
   const memberData = data?.result ?? [];
   const [memberCount, setMemberCount] = useState(0);
+  const [isReported, setIsReported] = useState(false);
   const memberStatus = useMemberStore((state) => state.memberInfo.memberStatus);
   const [isAlreadyEntry, setIsAlreadyEntry] = useState<boolean>(true);
   const isMemberData = memberData?.length > 0;
 
   const reportBottomSheet = useBottomSheet();
-  const reportSnapPoints = useMemo(() => ['80%'], []);
-  const reportedMemberId = memberData[memberCount]?.memberBookId ?? 0;
+  const reportSnapPoints = useMemo(() => ['75%'], []);
+  const reportedMemberId = memberData[memberCount]?.memberId ?? 0;
 
   useEffect(() => {
     const fetchOnboardingStatus = async () => {
@@ -63,15 +65,20 @@ const Home = () => {
       <S.Wrapper>
         {!isAlreadyEntry && <HomeOnboardingModal onClose={toggle} visible={isOpen} />}
         {EMemberStatus.MATCHING_DISABLED === memberStatus && <Lock />}
-        {!isLoading && isMemberData && (
+        {!isLoading && isMemberData && !isReported && (
           <MemberCard memberData={memberData[memberCount]} handleReport={reportBottomSheet.handleOpenBottomSheet} />
         )}
         {!isLoading && !isMemberData && <EventCard />}
+        {!isLoading && isReported && <InviteCard />}
 
         {/* <InviteCard /> */}
         {!isLoading && <Advert />}
         <CustomBottomSheetModal ref={reportBottomSheet.bottomRef} index={0} snapPoints={reportSnapPoints}>
-          <ReportOption bottomClose={reportBottomSheet.handleCloseBottomSheet} reportedMemberId={reportedMemberId} />
+          <ReportOption
+            bottomClose={reportBottomSheet.handleCloseBottomSheet}
+            reportedMemberId={reportedMemberId}
+            setIsReported={setIsReported}
+          />
         </CustomBottomSheetModal>
       </S.Wrapper>
     </>
