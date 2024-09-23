190344d8bd24f367d84a1e304c808e7099dd6946
bamb14 <118671229+bamb14@users.noreply.github.com>
2024-09-24 01:36:47 +0900

    feat: 온보딩 모달 수정, 친구초대보상 api 추가

---
+++ src/commons/api/modal/modal.api.ts
@@ -0,0 +1,11 @@
+import { Get, Post } from '@commons/configs/axios/http.api';
+import { MemberInvitationRewardResponse, MemberOnboardingStatusResponse } from '@commons/types/openapiGenerator';
+
+export const getOnboardingStatus = async () => Get<MemberOnboardingStatusResponse>('member-modal/onboarding', {});
+
+export const postOnboardingStatus = async (where: string) => Post('member-modal/onboarding', { onboarding: where });
+
+export const getInvitationRewardStatus = async () =>
+  Get<MemberInvitationRewardResponse>('member-modal/invitation-reward');
+
+export const postInvitationRewardStatus = async () => Post('member-modal/invitation-reward');
--- src/commons/api/onboarding/onboarding.api.ts
+++
@@ -1,5 +0,0 @@
-import { Get, Post } from '@commons/configs/axios/http.api';
-
-export const getOnboardingStatus = async () => Get('member-modal/onboarding', {});
-
-export const postOnboardingStatus = async (where: string) => Post('member-modal/onboarding', { onboarding: where });
