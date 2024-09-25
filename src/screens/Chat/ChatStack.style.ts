758f8f7254da7d1396e32dae30fb2e5c6741c3d0
Hangyeol-SSeo <dongha313@gmail.com>
2024-09-25 16:21:57 +0900

    feat: 채팅 로그인 및 로딩 화면 구현

---
+++ src/screens/Chat/ChatStack.style.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+
+export const LoadingWrapper = styled.View`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Spinner = styled.View`
+  width: 70px;
+  height: 70px;
+  box-sizing: border-box;
+  border: 3px solid rgba(255, 255, 255, 0.3);
+  border-top-color: rebeccapurple;
+  border-radius: 100px;
+  animation: spin 1s ease-in-out infinite;
+`;
--- src/screens/Chat/ChatStack.tsx
+++ src/screens/Chat/ChatStack.tsx
@@ -1,17 +1,20 @@
-import { Pressable, Text, View } from 'react-native';
-import { MMKV } from 'react-native-mmkv';
-import { platformServices } from '@screens/Chat/NativeModule';
+import { Animated } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useNavigation, useRoute, NavigationContainer } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import {
-  SendbirdUIKitContainer,
   useSendbirdChat,
   createGroupChannelListFragment,
   createGroupChannelCreateFragment,
   createGroupChannelFragment,
   useConnection,
+  SendbirdUIKitContainer,
 } from '@sendbird/uikit-react-native';
 import { useGroupChannel } from '@sendbird/uikit-chat-hooks';
+import React, { useEffect, useRef } from 'react';
+import { platformServices } from '@screens/Chat/NativeModule';
+import { MMKV } from 'react-native-mmkv';
+import { LoadingWrapper, Spinner } from '@screens/Chat/ChatStack.style';
+import { Easing } from 'react-native-reanimated';
 
 const GroupChannelListFragment = createGroupChannelListFragment();
 const GroupChannelCreateFragment = createGroupChannelCreateFragment();
@@ -79,48 +82,55 @@ const GroupChannelScreen = () => {
 
 const SignInScreen = () => {
   const { connect } = useConnection();
+  connect('blabla', { nickname: 'Hangyeol Seo' });
+
+  const spinValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(spinValue, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true, // This enables hardware acceleration
+        easing: Easing.inOut(Easing.ease),
+      }),
+    ).start();
+  }, [spinValue]);
+
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  });
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Pressable
-        style={{
-          width: 120,
-          height: 30,
-          backgroundColor: '#742DDD',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-        // TODO: Use the ID of a user you've created on the dashboard.
-        // If there isn't one, specify a unique ID so that a new user can be created with the value.
-        onPress={() => connect('blabla', { nickname: 'Hangyeol Seo' })}
-      >
-        <Text>Sign in</Text>
-      </Pressable>
-    </View>
+    <LoadingWrapper>
+      <Animated.View style={{ transform: [{ rotate: spin }] }}>
+        <Spinner />
+      </Animated.View>
+    </LoadingWrapper>
   );
 };
 
-const mmkv = new MMKV();
-const RootStack = createNativeStackNavigator();
-
+const Stack = createNativeStackNavigator();
 const Navigation = () => {
   const { currentUser } = useSendbirdChat();
 
   return (
-    <RootStack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
       {!currentUser ? (
-        <RootStack.Screen name={'SignIn'} component={SignInScreen} />
+        <Stack.Screen name="SignIn" component={SignInScreen} />
       ) : (
         <>
-          <RootStack.Screen name={'GroupChannelList'} component={GroupChannelListScreen} />
-          <RootStack.Screen name={'GroupChannelCreate'} component={GroupChannelCreateScreen} />
-          <RootStack.Screen name={'GroupChannel'} component={GroupChannelScreen} />
+          <Stack.Screen name={'GroupChannelList'} component={GroupChannelListScreen} />
+          <Stack.Screen name={'GroupChannelCreate'} component={GroupChannelCreateScreen} />
+          <Stack.Screen name={'GroupChannel'} component={GroupChannelScreen} />
         </>
       )}
-    </RootStack.Navigator>
+    </Stack.Navigator>
   );
 };
 
+const mmkv = new MMKV();
 export default function ChatStack() {
   return (
     <SendbirdUIKitContainer
