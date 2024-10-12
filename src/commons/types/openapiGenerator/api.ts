6eeb798820277c253b17871b68943a40b4610812
Hangyeol-SSeo <dongha313@gmail.com>
2024-10-13 01:02:33 +0900

    feat: api refetch

--- src/commons/types/openapiGenerator/api.ts
+++ src/commons/types/openapiGenerator/api.ts
@@ -427,19 +427,6 @@ export interface BookSearchResponses {
 /**
  * 
  * @export
- * @interface ChatRequest
- */
-export interface ChatRequest {
-    /**
-     * 
-     * @type {number}
-     * @memberof ChatRequest
-     */
-    'targetMemberId': number;
-}
-/**
- * 
- * @export
  * @interface ContactInfoResponse
  */
 export interface ContactInfoResponse {
@@ -512,6 +499,19 @@ export interface EmailResponse {
      * @memberof EmailResponse
      */
     'emailVerifyStatus'?: string;
+}
+/**
+ * 
+ * @export
+ * @interface EntryRequest
+ */
+export interface EntryRequest {
+    /**
+     * 
+     * @type {number}
+     * @memberof EntryRequest
+     */
+    'targetMemberId': number;
 }
 /**
  * 
@@ -2347,16 +2347,16 @@ export interface MyLibraryReadResponse {
 export interface NotificationResponse {
     /**
      * 
-     * @type {boolean}
-     * @memberof NotificationResponse
-     */
-    'success'?: boolean;
-    /**
-     * 
      * @type {string}
      * @memberof NotificationResponse
      */
     'status'?: string;
+    /**
+     * 
+     * @type {boolean}
+     * @memberof NotificationResponse
+     */
+    'success'?: boolean;
     /**
      * 
      * @type {string}
@@ -10132,14 +10132,14 @@ export const SendbirdApiAxiosParamCreator = function (configuration?: Configurat
         /**
          * 
          * @summary 채팅 수락
-         * @param {ChatRequest} chatRequest 
+         * @param {EntryRequest} entryRequest 
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
          */
-        chatAccept: async (chatRequest: ChatRequest, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
-            // verify required parameter 'chatRequest' is not null or undefined
-            assertParamExists('chatAccept', 'chatRequest', chatRequest)
-            const localVarPath = `/sendbird/chat`;
+        accept: async (entryRequest: EntryRequest, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'entryRequest' is not null or undefined
+            assertParamExists('accept', 'entryRequest', entryRequest)
+            const localVarPath = `/sendbird/entry/accept`;
             // use dummy base URL string because the URL constructor only accepts absolute URLs.
             const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
             let baseOptions;
@@ -10162,7 +10162,47 @@ export const SendbirdApiAxiosParamCreator = function (configuration?: Configurat
             setSearchParams(localVarUrlObj, localVarQueryParameter);
             let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
             localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
-            localVarRequestOptions.data = serializeDataIfNeeded(chatRequest, localVarRequestOptions, configuration)
+            localVarRequestOptions.data = serializeDataIfNeeded(entryRequest, localVarRequestOptions, configuration)
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+        /**
+         * 
+         * @summary 채팅 거절
+         * @param {EntryRequest} entryRequest 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        reject: async (entryRequest: EntryRequest, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'entryRequest' is not null or undefined
+            assertParamExists('reject', 'entryRequest', entryRequest)
+            const localVarPath = `/sendbird/entry/reject`;
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'POST', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            // authentication Bearer Authentication required
+            // http bearer authentication required
+            await setBearerAuthToObject(localVarHeaderParameter, configuration)
+
+
+    
+            localVarHeaderParameter['Content-Type'] = 'application/json';
+
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+            localVarRequestOptions.data = serializeDataIfNeeded(entryRequest, localVarRequestOptions, configuration)
 
             return {
                 url: toPathString(localVarUrlObj),
@@ -10216,14 +10256,27 @@ export const SendbirdApiFp = function(configuration?: Configuration) {
         /**
          * 
          * @summary 채팅 수락
-         * @param {ChatRequest} chatRequest 
+         * @param {EntryRequest} entryRequest 
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
          */
-        async chatAccept(chatRequest: ChatRequest, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<void>> {
-            const localVarAxiosArgs = await localVarAxiosParamCreator.chatAccept(chatRequest, options);
+        async accept(entryRequest: EntryRequest, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<void>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.accept(entryRequest, options);
             const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
-            const localVarOperationServerBasePath = operationServerMap['SendbirdApi.chatAccept']?.[localVarOperationServerIndex]?.url;
+            const localVarOperationServerBasePath = operationServerMap['SendbirdApi.accept']?.[localVarOperationServerIndex]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
+        },
+        /**
+         * 
+         * @summary 채팅 거절
+         * @param {EntryRequest} entryRequest 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async reject(entryRequest: EntryRequest, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<void>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.reject(entryRequest, options);
+            const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
+            const localVarOperationServerBasePath = operationServerMap['SendbirdApi.reject']?.[localVarOperationServerIndex]?.url;
             return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
         },
         /**
@@ -10251,12 +10304,22 @@ export const SendbirdApiFactory = function (configuration?: Configuration, baseP
         /**
          * 
          * @summary 채팅 수락
-         * @param {ChatRequest} chatRequest 
+         * @param {EntryRequest} entryRequest 
          * @param {*} [options] Override http request option.
          * @throws {RequiredError}
          */
-        chatAccept(chatRequest: ChatRequest, options?: any): AxiosPromise<void> {
-            return localVarFp.chatAccept(chatRequest, options).then((request) => request(axios, basePath));
+        accept(entryRequest: EntryRequest, options?: any): AxiosPromise<void> {
+            return localVarFp.accept(entryRequest, options).then((request) => request(axios, basePath));
+        },
+        /**
+         * 
+         * @summary 채팅 거절
+         * @param {EntryRequest} entryRequest 
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        reject(entryRequest: EntryRequest, options?: any): AxiosPromise<void> {
+            return localVarFp.reject(entryRequest, options).then((request) => request(axios, basePath));
         },
         /**
          * 
@@ -10280,13 +10343,25 @@ export class SendbirdApi extends BaseAPI {
     /**
      * 
      * @summary 채팅 수락
-     * @param {ChatRequest} chatRequest 
+     * @param {EntryRequest} entryRequest 
      * @param {*} [options] Override http request option.
      * @throws {RequiredError}
      * @memberof SendbirdApi
      */
-    public chatAccept(chatRequest: ChatRequest, options?: RawAxiosRequestConfig) {
-        return SendbirdApiFp(this.configuration).chatAccept(chatRequest, options).then((request) => request(this.axios, this.basePath));
+    public accept(entryRequest: EntryRequest, options?: RawAxiosRequestConfig) {
+        return SendbirdApiFp(this.configuration).accept(entryRequest, options).then((request) => request(this.axios, this.basePath));
+    }
+
+    /**
+     * 
+     * @summary 채팅 거절
+     * @param {EntryRequest} entryRequest 
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof SendbirdApi
+     */
+    public reject(entryRequest: EntryRequest, options?: RawAxiosRequestConfig) {
+        return SendbirdApiFp(this.configuration).reject(entryRequest, options).then((request) => request(this.axios, this.basePath));
     }
 
     /**
