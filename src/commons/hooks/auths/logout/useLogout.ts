32ef8cbc7b283b4b7213d11608bd67477aedbeec
Hangyeol-SSeo <dongha313@gmail.com>
2024-10-15 21:43:07 +0900

    feat: sendbird 푸시토큰 발급 과정 추가

--- src/commons/hooks/auths/logout/useLogout.ts
+++ src/commons/hooks/auths/logout/useLogout.ts
@@ -4,6 +4,9 @@ import { useStyleStore } from '@commons/store/members/style/useStyle';
 import { useUserStore } from '@commons/store/members/userinfo/useUserinfo';
 import useToastStore from '@commons/store/ui/toast/useToastStore';
 import { useSendbirdLogin } from '@commons/hooks/auths/successfulLogin/useSendbirdLogin';
+import { Platform } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import { useSendbirdChat } from '@sendbird/uikit-react-native';
 
 export const useLogout = (logoutToggle: () => void) => {
   const removeToken = useAuthStore((state) => state.removeToken);
@@ -12,11 +15,25 @@ export const useLogout = (logoutToggle: () => void) => {
   const resetStyleInfo = useStyleStore((state) => state.resetStyleInfo);
   const resetMemberInfo = useMemberStore((state) => state.resetMemberInfo);
   const { handleSendbirdLogout } = useSendbirdLogin();
+  const { sdk } = useSendbirdChat();
 
   const onClickLogout = () => {
     resetUserInfo();
     resetStyleInfo();
     resetMemberInfo();
+    // if (Platform.OS === 'ios') {
+    //   const token = await messaging().getAPNSToken();
+    //   messaging().onTokenRefresh(async (newToken: string) => {
+    //     console.debug('refresh token');
+    //   });
+    //   await sdk.unregisterAPNSPushTokenForCurrentUser(token ?? '');
+    // } else {
+    //   const token = await messaging().getToken();
+    //   messaging().onTokenRefresh(async (newToken: string) => {
+    //     console.debug('refresh token');
+    //   });
+    //   await sdk.unregisterFCMPushTokenForCurrentUser(token);
+    // }
     handleSendbirdLogout();
     logoutToggle();
     showToast({
