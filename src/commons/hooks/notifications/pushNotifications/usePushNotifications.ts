95ca21ff9ea37bfe8dbdd1cf4258389c27605684
Hangyeol-SSeo <dongha313@gmail.com>
2024-10-18 00:34:55 +0900

    feat: background notification 추가

--- src/commons/hooks/notifications/pushNotifications/usePushNotifications.ts
+++ src/commons/hooks/notifications/pushNotifications/usePushNotifications.ts
@@ -1,11 +1,33 @@
-import { Platform } from 'react-native';
-import * as TaskManager from 'expo-task-manager';
-import * as Notifications from 'expo-notifications';
-import { isSendbirdNotification, parseSendbirdNotification } from '@sendbird/uikit-utils';
 import { useEffect } from 'react';
+import * as Notifications from 'expo-notifications';
+import * as TaskManager from 'expo-task-manager';
+import messaging from '@react-native-firebase/messaging';
+import { parseSendbirdNotification, isSendbirdNotification } from '@sendbird/uikit-utils';
+import { Platform } from 'react-native';
 
 const usePushNotifications = () => {
   useEffect(() => {
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+      const { data } = remoteMessage; // 메시지 데이터에서 'data' 부분 추출
+
+      if (isSendbirdNotification(data)) {
+        const sendbirdPayload = parseSendbirdNotification(data); // 'sendbird' 데이터 파싱
+
+        console.log('Parsed Sendbird Notification: ', sendbirdPayload);
+
+        // 백그라운드에서 알림 표시
+        await Notifications.scheduleNotificationAsync({
+          content: {
+            title: `${sendbirdPayload.channel.name} 새로운 메시지}`, // 알림 제목
+            body: sendbirdPayload.message, // 알림 본문
+            data: sendbirdPayload, // 알림의 추가 데이터
+          },
+          trigger: null, // 즉시 알림을 띄움
+        });
+      }
+    });
+
     Notifications.setNotificationHandler({
       handleNotification: async () => ({
         shouldShowAlert: true,
@@ -13,25 +35,33 @@ const usePushNotifications = () => {
         shouldSetBadge: true,
       }),
     });
-    const onNotification = (notification: Notifications.NotificationResponse) => {
-      const data = notification?.notification?.request?.content?.data as any;
-      if (data && isSendbirdNotification(data)) {
-        const sendbird = parseSendbirdNotification(data);
-        // 채널 이동 등의 동작을 추가할 수 있음
-        console.log('Sendbird notification received in foreground:', sendbird);
-      }
-    };
 
-    // 앱이 열릴 때 마지막으로 수신한 알림 확인
-    const checkAppOpenedWithNotification = async () => {
-      const response = await Notifications.getLastNotificationResponseAsync();
-      if (response) {
-        onNotification(response);
-      }
-    };
+    // NOTE: 알림을 사용자가 탭했을 때 호출될 리스너
+    const responseListener = Notifications.addNotificationResponseReceivedListener((response) => {
+      const data = response.notification.request.content.data;
 
-    const foregroundSubscription = Notifications.addNotificationResponseReceivedListener(onNotification);
-    checkAppOpenedWithNotification();
+      if (isSendbirdNotification(data)) {
+        const sendbirdPayload = parseSendbirdNotification(data);
+      }
+    });
+
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      const data = remoteMessage.data;
+      alert(JSON.stringify(data));
+
+      if (isSendbirdNotification(data)) {
+        const sendbirdPayload = parseSendbirdNotification(data);
+        alert(JSON.stringify(sendbirdPayload));
+        await Notifications.scheduleNotificationAsync({
+          content: {
+            title: `[Sendbird] ${sendbirdPayload.channel.name}`,
+            body: sendbirdPayload.message,
+            data: sendbirdPayload,
+          },
+          trigger: null,
+        });
+      }
+    });
 
     if (Platform.OS === 'android') {
       // Set channel
@@ -43,20 +73,21 @@ const usePushNotifications = () => {
 
       // Set background message handler
       const BACKGROUND_NOTIFICATION_TASK = 'BACKGROUND-NOTIFICATION-TASK';
-      TaskManager.defineTask(BACKGROUND_NOTIFICATION_TASK, ({ data }) => {
+      TaskManager.defineTask(BACKGROUND_NOTIFICATION_TASK, ({data}) => {
         if (Platform.OS !== 'android') return;
 
         console.log('Background notification task', data);
 
         if (isSendbirdNotification(data)) {
           const sendbird = parseSendbirdNotification(data);
+          console.log('sendbird', sendbird);
 
           Notifications.scheduleNotificationAsync({
             identifier: String(sendbird.message_id),
             content: {
               title: `[RN]${sendbird.channel.name || sendbird.sender?.name || 'Message received'}`,
               body: sendbird.message,
-              data,
+              data: data,
             },
             trigger: null,
           });
@@ -66,7 +97,9 @@ const usePushNotifications = () => {
     }
 
     return () => {
-      foregroundSubscription.remove();
+      alert('notification return');
+      Notifications.removeNotificationSubscription(responseListener);
+      unsubscribe();
     };
   }, []);
 };
