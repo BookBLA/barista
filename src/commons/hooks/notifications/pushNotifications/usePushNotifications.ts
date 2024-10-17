8e157a697aaedba7f5c3ad88db4e4be0c87e34a8
Hangyeol-SSeo <dongha313@gmail.com>
2024-10-18 01:47:25 +0900

    feat: background notification

--- src/commons/hooks/notifications/pushNotifications/usePushNotifications.ts
+++ src/commons/hooks/notifications/pushNotifications/usePushNotifications.ts
@@ -1,29 +1,36 @@
 import { useEffect } from 'react';
 import * as Notifications from 'expo-notifications';
-import * as TaskManager from 'expo-task-manager';
 import messaging from '@react-native-firebase/messaging';
 import { parseSendbirdNotification, isSendbirdNotification } from '@sendbird/uikit-utils';
 import { Platform } from 'react-native';
 
 const usePushNotifications = () => {
+  if (Platform.OS === 'android') {
+    const channelId = `${process.env.EXPO_PUBLIC_SENDBIRD_APP_ID}`;
+    Notifications.setNotificationChannelAsync(channelId, {
+      name: 'Sendbird Notification',
+      importance: Notifications.AndroidImportance.HIGH,
+    });
+  }
+
   useEffect(() => {
-    messaging().setBackgroundMessageHandler(async remoteMessage => {
-      console.log('Message handled in the background!', remoteMessage);
+    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+      // console.log('Message handled in the background!', remoteMessage);
       const { data } = remoteMessage; // 메시지 데이터에서 'data' 부분 추출
 
       if (isSendbirdNotification(data)) {
         const sendbirdPayload = parseSendbirdNotification(data); // 'sendbird' 데이터 파싱
 
-        console.log('Parsed Sendbird Notification: ', sendbirdPayload);
+        // console.log('Parsed Sendbird Notification: ', sendbirdPayload);
 
         // 백그라운드에서 알림 표시
         await Notifications.scheduleNotificationAsync({
           content: {
-            title: `${sendbirdPayload.channel.name} 새로운 메시지}`, // 알림 제목
-            body: sendbirdPayload.message, // 알림 본문
-            data: sendbirdPayload, // 알림의 추가 데이터
+            title: `${sendbirdPayload.sender?.name}님의 새 메시지`,
+            body: sendbirdPayload.message,
+            data: sendbirdPayload,
           },
-          trigger: null, // 즉시 알림을 띄움
+          trigger: null,
         });
       }
     });
@@ -47,14 +54,13 @@ const usePushNotifications = () => {
 
     const unsubscribe = messaging().onMessage(async (remoteMessage) => {
       const data = remoteMessage.data;
-      alert(JSON.stringify(data));
 
       if (isSendbirdNotification(data)) {
         const sendbirdPayload = parseSendbirdNotification(data);
-        alert(JSON.stringify(sendbirdPayload));
+        // alert(JSON.stringify(sendbirdPayload));
         await Notifications.scheduleNotificationAsync({
           content: {
-            title: `[Sendbird] ${sendbirdPayload.channel.name}`,
+            title: `${sendbirdPayload.sender?.name}님의 새 메시지`,
             body: sendbirdPayload.message,
             data: sendbirdPayload,
           },
@@ -63,41 +69,7 @@ const usePushNotifications = () => {
       }
     });
 
-    if (Platform.OS === 'android') {
-      // Set channel
-      const channelId = 'default';
-      Notifications.setNotificationChannelAsync(channelId, {
-        name: 'Default Channel',
-        importance: 4,
-      });
-
-      // Set background message handler
-      const BACKGROUND_NOTIFICATION_TASK = 'BACKGROUND-NOTIFICATION-TASK';
-      TaskManager.defineTask(BACKGROUND_NOTIFICATION_TASK, ({data}) => {
-        if (Platform.OS !== 'android') return;
-
-        console.log('Background notification task', data);
-
-        if (isSendbirdNotification(data)) {
-          const sendbird = parseSendbirdNotification(data);
-          console.log('sendbird', sendbird);
-
-          Notifications.scheduleNotificationAsync({
-            identifier: String(sendbird.message_id),
-            content: {
-              title: `[RN]${sendbird.channel.name || sendbird.sender?.name || 'Message received'}`,
-              body: sendbird.message,
-              data: data,
-            },
-            trigger: null,
-          });
-        }
-      });
-      Notifications.registerTaskAsync(BACKGROUND_NOTIFICATION_TASK);
-    }
-
     return () => {
-      alert('notification return');
       Notifications.removeNotificationSubscription(responseListener);
       unsubscribe();
     };
